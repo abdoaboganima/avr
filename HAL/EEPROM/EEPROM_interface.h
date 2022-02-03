8134259f0c87517c35c237742d77bd9858bf9b4a
Abdo <abdoaboganima@icloud.com>
2022-02-03 16:56:07 +0200

    Added two functions in the `EEPROM module` for reading blocks of data to and from the EEPROM. Made some refactors

--- HAL/EEPROM/EEPROM_interface.h
+++ HAL/EEPROM/EEPROM_interface.h
@@ -31,5 +31,32 @@ void EEPROM_sendByte(const uint8_t data, const uint16_t address);
  */
 void EEPROM_readByte(uint8_t * const var, const uint16_t address);
 
+ /**
+ * @brief Send sequence of data addressed by `sequence` to the EEPROM across the I^{2}C bus
+ * @bug Sending more than 12 bytes across the I2C bus causes errors.
+ * @param sequence 
+ * @param firstAddress 
+ * @param sequenceSize 
+ * @note The data that will be stored is addressed by `sequence` and `fisrtAddress` is
+ * the first address in the EEPROM of the data that will be stored
+ * @code EEPROM_sendSequence("CoolStuff", 10, 9);
+ * @endcode 
+ */
+void EEPROM_sendSequence(uint8_t * const sequence, const uint16_t firstAddress, const uint16_t sequenceSize);
+
+ /**
+ * @brief Read sequence of data from the EEPROM and store them in a memory location addressed by `sequence`
+ * @param sequence 
+ * @param firstAddress 
+ * @param sequenceSize 
+ * @note In the code bellow the data will be stored in the `buff` starting from the
+ * address 20 in the EEPROM till address 1019 
+ * @code 
+ * uint8_t buff[1000];
+ * EEPROM_readSequence(buff, 20, 1000);
+ * @endcode 
+ */
+void EEPROM_readSequence(uint8_t * const sequence, const uint16_t firstAddress, const uint16_t sequenceSize);
+
 #endif /* _EEPROM_INTERFACE_H_ */
 /*@}@}*/
--- HAL/EEPROM/EEPROM_program.c
+++ HAL/EEPROM/EEPROM_program.c
@@ -1,3 +1,12 @@
+ /**
+ * @file EEPROM_program.c
+ * @author Abdulrahman Aboghanima (abdoaboganima@icloud.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-03
+ * @todo Fix or remove the delays. Include the in the I2C driver
+ * @copyright Copyright (c) 2022
+ */
 #include "EEPROM_interface.h"
 #include "EEPROM_config.h"
 #include "../../MCAL/TWI/TWI_interface.h"
@@ -12,16 +21,24 @@ So in order to access that location you have to through the following address in
 
 */
 
-void EEPROM_sendByte(const uint8_t data, const uint16_t address)
+static inline void EEPROM_sendAddress(const uint16_t address)
 {
   TWI_sendStartCondition();
+  _delay_ms(50);
   /*The address of the `AT24C08A` consists of 3 pats
       1- 1010 Selects the `AT24C08A`
       2- A2 select which `AT24C08A` you will communicate with, you can connect up to two
       3  The two LSB selects the block number in the memory 
   */
   TWI_sendSlaveAddressWithWrite( 0b1010000 | (A2_PIN<<2) |(address>>8) );
+  _delay_ms(50);
   TWI_masterWriteDataByte( (uint8_t) address);
+  _delay_ms(50);
+}
+
+void EEPROM_sendByte(const uint8_t data, const uint16_t address)
+{
+  EEPROM_sendAddress(address);
   TWI_masterWriteDataByte(data);
   TWI_sendStopCondition();
   _delay_ms(50);
@@ -29,12 +46,39 @@ void EEPROM_sendByte(const uint8_t data, const uint16_t address)
 
 void EEPROM_readByte(uint8_t * const var, const uint16_t address)
 {
-  TWI_sendStartCondition();
-  TWI_sendSlaveAddressWithWrite( 0b1010000 | (A2_PIN<<2) | (address>>8) );
-  TWI_masterWriteDataByte((uint8_t)address);
+  EEPROM_sendAddress(address);
+  _delay_ms(50);
   TWI_sendRepeatedStartCondition();
+  _delay_ms(50);
   TWI_sendSlaveAddressWithRead( 0b1010000 | (A2_PIN<<2) | (address>>8) );
+  _delay_ms(50);
   TWI_masterReadDataByte(var);
+  _delay_ms(50);
   TWI_sendStopCondition();
-  
+  _delay_ms(50);
+}
+
+void EEPROM_sendSequence(uint8_t * const sequence, const uint16_t firstAddress, const uint16_t sequenceSize)
+{
+  EEPROM_sendAddress(firstAddress);
+  for(uint16_t i=0; i<sequenceSize; i++){ 
+    TWI_masterWriteDataByte(sequence[i]);
+    _delay_ms(50);
+  }
+ 
+  TWI_sendStopCondition();
+  _delay_ms(50);
+
+}
+
+void EEPROM_readSequence(uint8_t * const sequence, const uint16_t firstAddress, const uint16_t sequenceSize)
+{
+  EEPROM_sendAddress(firstAddress);
+  TWI_sendRepeatedStartCondition();
+  TWI_sendSlaveAddressWithRead( 0b1010000 | (A2_PIN<<2) | (firstAddress>>8) );
+  for(uint16_t i=0; i<sequenceSize; i++)
+    TWI_masterReadDataByte(sequence+i);
+ 
+  TWI_sendStopCondition();
+  _delay_ms(50);
 }
